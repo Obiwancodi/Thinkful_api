3838673d80d4f4171e7f4863db274788f3137688
George Fleming <gflemingiii@gmail.com>
2015-02-09 21:29:17 +0000

    Query Strings added

--- posts/api.py
+++ posts/api.py
@@ -8,3 +8,67 @@ import decorators
 from posts import app
 from database import session
 
+@app.before_request
+def before_request():
+    method = request.form.get('_method', '').upper()
+    if method:
+        request.environ['REQUEST_METHOD'] = method
+        ctx = flask._request_ctx_stack.top
+        ctx.url_adapter.default_method = method
+        assert request.method == method
+
+@app.route("/api/posts", methods=["GET"])
+@decorators.accept("application/json")
+def posts_get():
+    """ Get a list of posts """
+    # Get the querystring arguments
+    title_like = request.args.get("title_like")
+    body_like = request.args.get("body_like")
+
+    # Get and filter the posts from the database
+    posts = session.query(models.Post)
+    if title_like and body_like:
+        posts = posts.filter(models.Post.title.contains(title_like),models.Post.body.contains(body_like))
+    elif title_like:
+        posts = posts.filter(models.Post.title.contains(title_like))
+    elif body_like:
+        posts = posts.filter(models.Post.body.contains(body_like))
+    posts = posts.all()
+    
+    # Convert the posts to JSON and return a response
+    data = json.dumps([post.as_dictionary() for post in posts])
+    return Response(data, 200, mimetype="application/json")
+
+@app.route("/api/posts/<int:id>", methods=["GET"])
+@decorators.accept("application/json")
+def post_get(id):
+    """ Single post endpoint """
+    # Get the post from the database
+    post = session.query(models.Post).get(id)
+
+    # Check whether the post exists
+    # If not return a 404 with a helpful message
+    if not post:
+        message = "Could not find post with id {}".format(id)
+        data = json.dumps({"message": message})
+        return Response(data, 404, mimetype="application/json")
+
+    # Return the post as JSON
+    data = json.dumps(post.as_dictionary())
+    return Response(data, 200, mimetype="application/json")
+
+@app.route("/api/posts/<int:id>", methods=["DELETE"])
+@decorators.accept("application/json")
+def delete_post(id):
+    """ Delete post endpoint"""
+    post = session.query(models.Post).get(id)
+    if not post:
+        message = "Could not find post with id {}".format(id)
+        data = json.dumps({"message": message})
+        return Response(data, 404, mimetype="application/json")
+    
+    session.delete(post)
+    session.commit()
+    post = session.query(models.Post).get(id)
+    data = json.dumps(post)
+    return Response(data, 200, mimetype="application/json")
